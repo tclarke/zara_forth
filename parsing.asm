36d27cf6ec2364576aedb46c39149a9b7e0c1e2c
Trevor R.H. Clarke <trevor@notcows.com>
2023-04-13 01:35:04 -0400

    Big refactor but parse_name is working now

--- parsing.asm
+++ parsing.asm
@@ -7,91 +7,97 @@
     MODULE  parsing
 
 WHITESPACE  =       20h     ; SPACE
-NULL        =       0       ; NULL
 BUFFER_LEN  =       255     ; Maximum buffer length
 
-; Input buffer and current length
-ib_len:       BYTE      0
+; Input buffer and current length.
+; These are WORDs so we can treat them as cells for >IN, etc.
+; But we can always read and write ib_len+1 and parse_idx+1 for faster access
+; Since the high byte will always be 0 as the maximum buffer length is 255
+ib_len:       WORD      0
+parse_idx:    WORD      0
 input_buffer: BLOCK     BUFFER_LEN
 
-; Parse area, >IN, and current length
-; Double buffer to minimize dropped input characters
-pa_idx:       BYTE      0
-pa_len:       BYTE      0
-parse_area:   BLOCK     BUFFER_LEN
-
 ;
-; Skip whitespace
+; Find the next whitespace character from the current address through the EOB
 ;
-; @param hl  - Address of the buffer
-; @param c   - Maximum length of the buffer.
-; @return hl - Address of the first byte of the token
-; @return z  - Clear if whitespace was found, set otherwise
+; @param hl  - Address of the first character to check
+; @param bc  - Remaining byte count in the buffer
+;
+; @return hl - The address of the next whitespace character or invalid if none found.
+; @return bc - Remainind byte count in the buffer
+;
+find_whitespace:
+    ld      a, WHITESPACE
+1:  cpi                 ; this always incs hl and decs bc
+    jr      z, 2F       ; character is whitespace
+    ret     po          ; character isn't whitespace and we've hit the EOB
+    jr      1B          ; keep searching
+2:  dec     hl          ; we want to point at the first whitespace character so undo the cpi inc
+    inc     c
+    ret
+    
+;
+; Skip all whitespace from the current address.
+;
+; @param hl  - Address of the first character to check
+; @param bc  - Remaining byte count in the buffer
+;
+; @return hl - Address of the next non-whitespace character in the buffer or EOB + 1 if none found
+; @return bc - Remainind byte count in the buffer
 ;
 skip_whitespace:
-    ld          b, 0            ; cpir uses the 16-bit bc but we're only allowing 8-bit lengths
-    ld          a, WHITESPACE
-    cpir
-    ld          a, c
-    cp          0               ; Check if we hit the end of the buffer so z is set correctly
+    ld      a, WHITESPACE
+1:  cpi
+    jr      nz, 2F      ; character is not whitespace
+    ret     po          ; character is whitespace and we've hit EOB
+    jr      1B          ; continue
+2:  dec     hl
+    inc     c
     ret
 
 ;
-; Compare two NULL terminated strings.
+; Parse the input buffer for a name.
 ;
-; @param hl  - The address of the first string
-; @param de  - The address of the second string
-; @return z  - clear if the strings are equal, set if they are not
-; @return hl - The pointer to the last compared character in string 1
-; @return de - The pointer to the last compared character in string 2
+; This will update parse_idx
 ;
-strcmp:
-    ld      a, (hl)     ; load the next character of the first string
-    ld      b, a
-    ld      a, (de)     ; load the next charater of the second string
-    cp      b
-    ret     nz          ; return if strings are not equal
-    cp      NULL
-    ret     z           ; return if NULL..since we know both strings are equal at
-                        ; this point, this indicates they are both at the end of the string
-    inc     hl          ; move to the next characters
-    inc     de
-    jr      strcmp
-
+; @return hl - The address of the start of the name
+; @return a  - The length of the name
 ;
-; Checks to see if the character is a digit or not.
-;
-; @param a  - The character byte to check
-; @return z - clear if the character is a digit, set otherwise
-;
-isdigit:
-    sub     '0'
-    jp      s, 1F       ; if a was < the 0 ascii code then we aren't a digit
-    sub     10
-    jp      ns, 1F      ; if a was > the 9 ascii code then we aren't a digit
-    ld      a, 0
-    cp      1           ; clear the Z flag
-    ret
-1   ld      a, 1
-    cp      1           ; set the Z flag
-    ret
-
-;
-; Macro to copy the input buffer to the parse area
-; Clobbers hl, de, and bc
-    MACRO .COPY2PARSE
+parse_name:
+    di                  ; disable interrupts for this routine since they can update the buffer
+    ; adjust hl bc to account for the parse_idx
     ld      hl, input_buffer
-    ld      de, parse_area
-    ld      bc, ib_len
-    ldir
-    ENDM
+    ld      de, (parse_idx)
+    add     hl, de
+    ld      a, (ib_len)
+    sub     e
+    ld      c, a
+    ld      b, 0h
 
-;
-; Locate the start and end of a token.
-;
-tokenize:
-    ld      hl, parse_area
-    ld      bc, pa_idx
-    add     hl, bc
+    call    skip_whitespace
+    push    hl          ; store this so we can return it later
+
+    ld      d, c        ; store this so we can calculate the length
+    call    find_whitespace
+
+    ld      a, (ib_len)
+    sub     c               ; buf len - remaining will be the new parse_idx
+    jr      c, 1F           ; reached end of buffer so c > a
+    inc     a               ; need to include 1 more since we already checked for a whitespace char
+    ld      (parse_idx), a
+
+    ; calculate the size of the token
+    ld      a, d
+    sub     c
+    pop     hl
+    ei
+    ret
+1:  ld      a, (ib_len)
+    ld      (parse_idx), a
+    ld      a, 0
+    pop     hl
+    ei
+    ret
+
 
     ENDMODULE
--- unit_tests.asm
+++ unit_tests.asm
@@ -24,71 +24,97 @@
 
     MODULE TestSuite_Parsing     ; Tests parsing
 
-test_str1:  byte     " 1 22 +\N"
+test_str1:  byte    "123 45 -\N"     ; bog standard input string
+len1:       equ     9
+test_str2:  byte    " 1 22 +\N"      ; starts with a space
+len2:       equ     10
+test_str3:  byte    "1 22 +"         ; doesn't end in a newline
+len3:       equ     6
+test_str4:  byte    "1 21  +\N"      ; 2 spaces in a row
+len4:       equ     8
+test_str5:  byte    "1234\n"         ; no whitespace
+len5:       equ     5
+
+; TODO: UT_FindWhitespace and UT_SkipWhitespace should have
+; ASSERT HL == test_str1+3
+; tests, but I keep getting "test_str1 label not found".
+UT_FindWhitespace:
+    ld      hl, test_str1
+    ld      bc, len1
+    call    parsing.find_whitespace
+    nop ; ASSERTION BC == 6
+
+    ld      hl, test_str2
+    ld      bc, len2
+    call    parsing.find_whitespace
+    nop ; ASSERTION BC == 10
+
+    ld      hl, test_str5
+    ld      bc, len5
+    call    parsing.find_whitespace
+    nop ; ASSERTION BC == 0
+
+    TC_END
+
 UT_SkipWhitespace:
-    ld      hl, test_str1
-    ld      c, 8
+    ld      hl, test_str2
+    ld      bc, len2
     call    parsing.skip_whitespace
-    TEST_MEMORY_BYTE hl, '1'
-    TEST_FLAG_NZ
+    nop ; ASSERTION BC == 9
 
+    ld      hl, test_str4
+    ld      bc, 4           ; set the index at the double whitespace
+    add     hl, bc
+    ld      a, len4
+    sub     c
+    ld      c, a
     call    parsing.skip_whitespace
-    TEST_MEMORY_BYTE hl, '2'
-    TEST_FLAG_NZ
+    nop ; ASSERTION BC == 2
 
-    call    parsing.skip_whitespace
-    TEST_MEMORY_BYTE hl, '+'
-    TEST_FLAG_NZ
-
-    call    parsing.skip_whitespace
-    TEST_FLAG_Z
     TC_END
 
-test_str2:  byte     " 1 22 +\0"
-test_str3:  byte     " 1 21 +\N\0"
-UT_StrCmp:
-    ld      hl, test_str1
-    ld      de, test_str1
-    call    parsing.strcmp
-    TEST_FLAG_Z
-    TEST_MEMORY_BYTE hl, 0
-    TEST_MEMORY_BYTE de, 0
+    MACRO   SETUP_PARSENAME  str?, n?
+    ld      hl, str?
+    ld      de, parsing.input_buffer
+    ld      bc, n?
+    ldir
+    ld      a, 0
+    ld      (parsing.parse_idx), a
+    ld      a, n?
+    ld      (parsing.ib_len), a
+    ENDM
 
-    ld      hl, test_str1
-    ld      de, test_str2
-    call    parsing.strcmp
-    TEST_FLAG_NZ
-    TEST_MEMORY_BYTE hl, 0ah  ; NL
-    TEST_MEMORY_BYTE de, 0
+UT_ParseName:
+    SETUP_PARSENAME test_str1, len1
+    call   parsing.parse_name
+    nop ; ASSERTION A == 3
+    TEST_MEMORY_WORD    parsing.parse_idx, 4
+    call   parsing.parse_name
+    nop ; ASSERTION A == 2
+    TEST_MEMORY_WORD    parsing.parse_idx, 7
 
-    ld      hl, test_str1
-    ld      de, test_str3
-    call    parsing.strcmp
-    TEST_FLAG_NZ
-    TEST_MEMORY_BYTE hl, 32h  ; '2'
-    TEST_MEMORY_BYTE de, 31h  ; '1'
-    TC_END
+    SETUP_PARSENAME test_str2, len2
+    call   parsing.parse_name
+    nop ; ASSERTION A == 1
+    TEST_MEMORY_WORD    parsing.parse_idx, 3
+    call   parsing.parse_name
+    nop ; ASSERTION A == 2
+    TEST_MEMORY_WORD    parsing.parse_idx, 6
 
-UT_IsDigit:
-    ld      a, '0'
-    call parsing.isdigit
-    TEST_FLAG_NZ
+    SETUP_PARSENAME test_str3, len3
+    call   parsing.parse_name
+    nop ; ASSERTION A == 1
+    TEST_MEMORY_WORD    parsing.parse_idx, 2
+    call   parsing.parse_name
+    nop ; ASSERTION A == 2
+    TEST_MEMORY_WORD    parsing.parse_idx, 5
+    call   parsing.parse_name
+    nop ; ASSERTION A == 1
+    TEST_MEMORY_WORD    parsing.parse_idx, 7
+    call   parsing.parse_name
+    nop ; ASSERTION A == 0
+    TEST_MEMORY_WORD    parsing.parse_idx, len3
 
-    ld      a, '5'
-    call parsing.isdigit
-    TEST_FLAG_NZ
-
-    ld      a, '9'
-    call parsing.isdigit
-    TEST_FLAG_NZ
-
-    ld      a, '/'
-    call parsing.isdigit
-    TEST_FLAG_Z
-
-    ld      a, ':'
-    call parsing.isdigit
-    TEST_FLAG_Z
     TC_END
 
     ENDMODULE
